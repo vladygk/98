89d6ec1ee344768bfc98bc1b0dbc52c214afc67d
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-16 23:13:47 +0200

    Fix"moved login form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import {LoginForm} from "./components/LoginForm.js" 
 function App() {
   return (
-    <div className="App">
-      <LoginForm/>
-    </div>
+    <LoginForm/>
   );
 }
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,12 @@
 import styles from "./LoginForm.module.css";
 
-export function LoginForm(props){
-    return (<form className={styles.form}>
+export function LoginForm(){
+    return (
+    <form className={styles.form}>
         <label className={styles.label} name="username">Username</label>
         <input className={styles.input} type="text" name="username"></input>
         <label className={styles.label} name="password">Password</label>
         <input className={styles.input} type="password" name="password"></input>
         <button className={styles.submit}>Sumbit</button>
-    </form>)
+    </form>);
 }
