1014e29defb228677d5bc4a985db3c1b2aaacd30
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-16 23:21:06 +0200

    Remove: label

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,9 +3,8 @@ import styles from "./LoginForm.module.css";
 export function LoginForm(){
     return (
     <form className={styles.form}>
-        <label className={styles.label} name="username">Username</label>
+        <label className={styles.label} >LoginForm</label>
         <input className={styles.input} type="text" name="username"></input>
-        <label className={styles.label} name="password">Password</label>
         <input className={styles.input} type="password" name="password"></input>
         <button className={styles.submit}>Sumbit</button>
     </form>);
