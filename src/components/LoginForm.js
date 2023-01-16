d98045ead88bb3878cfa380ab8c0de0537491e07
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-16 23:29:18 +0200

    add: htmlFor

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,19 @@
 import styles from "./LoginForm.module.css";
 
-export function LoginForm(){
-    return (
+export function LoginForm() {
+  return (
     <form className={styles.form}>
-        <label className={styles.label} >LoginForm</label>
-        <input className={styles.input} type="text" name="username"></input>
-        <input className={styles.input} type="password" name="password"></input>
-        <button className={styles.submit}>Sumbit</button>
-    </form>);
+      <label className={styles.label} htmlFor="username">
+        Username
+      </label>
+      <input className={styles.input} type="text" id="username"></input>
+
+      <label className={styles.label} htmlFor="password">
+        Password
+      </label>
+      <input className={styles.input} type="password" id="password"></input>
+
+      <button className={styles.submit}>Sumbit</button>
+    </form>
+  );
 }
